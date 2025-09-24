e8e229909d18bab5c8db9a351de1606590e8c668
Krishal Ale <alekrishal@gmail.com>
2025-09-24 16:22:08 +0545

    admin on the way

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -83,6 +83,22 @@ const registerUser = async (req,res) => {
 // Route for admin login
 const adminLogin = async (req,res) => {
 
+  const {email,password} = req.body;
+
+  if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+    const token = jwt.sign(email+password,process.env.JWT_SECRET);
+    res.json({success:true,token})
+  }else{
+    res.json({success:false,message:"Invalid Credentials"})
+  }
+
+  try{
+
+  }catch(error){
+    console.log(error)
+    res.json({success:false,message:error.message})
+  }
+
 }
 
 
